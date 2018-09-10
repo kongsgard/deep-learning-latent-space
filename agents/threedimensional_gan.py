00136b13038fb970ef08508086d584858065b462
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2018-09-10 16:15:05 +0200

    Plot shape for each epoch

--- agents/threedimensional_gan.py
+++ agents/threedimensional_gan.py
@@ -1,9 +1,11 @@
+from tqdm import tqdm
+
 import torch
-import torchvision
 import visdom
 
 from agents.base import BaseAgent
 from datasets.shapenet import ShapeNetDataLoader
+from utils.misc import print_cuda_statistics
 from utils.voxel_utils import plot_voxels_in_visdom
 
 
@@ -14,10 +16,27 @@ class ThreeDimensionalGANAgent(BaseAgent):
         # Define dataloader
         self.dataloader = ShapeNetDataLoader(self.config)
 
-        # Plot first input
-        inputs = next(iter(self.dataloader.loader))
-        vis = visdom.Visdom()
-        plot_voxels_in_visdom(torch.Tensor.numpy(inputs[0]), vis)
+        # Initialize counter
+        self.current_epoch = 0
+        self.current_iteration = 0
+
+        # Set cuda flag
+        self.is_cuda = torch.cuda.is_available()
+        if self.is_cuda and not self.config.cuda:
+            self.logger.info("WARNING: You have a CUDA device - enable it!")
+        self.cuda = self.is_cuda & self.config.cuda
+
+        # Set the manual seed for torch
+        if self.cuda:
+            self.device = torch.device("cuda")
+            torch.cuda.set_device(self.config.gpu_device)
+            torch.cuda.manual_seed_all(self.config.seed)
+            self.logger.info("Program will run on ***GPU-CUDA***")
+            print_cuda_statistics()
+        else:
+            self.device = torch.device("cpu")
+            torch.manual_seed(self.config.seed)
+            self.logger.info("Program will run on ***CPU***")
 
     def load_checkpoint(self, file_name):
         pass
@@ -26,13 +45,27 @@ class ThreeDimensionalGANAgent(BaseAgent):
         pass
 
     def run(self):
-        pass
+        try:
+            self.train()
+
+        except KeyboardInterrupt:
+            self.logger.info("CTRL+C was pressed - wait to finalize...")
 
     def train(self):
-        pass
+        for epoch in range(self.current_epoch, self.config.max_epoch):
+            self.current_epoch = epoch
+            self.train_one_epoch()
+            self.save_checkpoint()
 
     def train_one_epoch(self):
-        pass
+        # Initialize tqdm batch
+        tqdm_batch = tqdm(self.dataloader.loader, total=self.dataloader.num_iterations,
+                          desc="epoch-{}-".format(self.current_epoch))
+
+        vis = visdom.Visdom()  # TODO: Move
+        for curr_it, x in enumerate(tqdm_batch):
+            if curr_it == 0:  # Todo: Refactor
+                plot_voxels_in_visdom(torch.Tensor.numpy(x[0]), vis)
 
     def validate(self):
         pass
--- utils/config.py
+++ utils/config.py
@@ -89,8 +89,6 @@ def process_config(json_file):
     # setup logging in the project
     setup_logging(config.log_dir)
 
-    logging.getLogger().info("Hi, This is root.")
-    logging.getLogger().info("After the configurations are successfully processed and dirs are created.")
-    logging.getLogger().info("The pipeline of the project will begin now.")
+    logging.getLogger().info("The project pipeline will begin")
 
     return config
