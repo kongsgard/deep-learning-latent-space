e7d111d74530d24825fdcbc59d99dea698b1b955
Sondre Kongsgård <sondre.kongsgaard@gmail.com>
2019-02-25 10:46:49 +0100

    Complete the encoder in PCN

--- dl_models/graphs/models/pcn_cd.py
+++ dl_models/graphs/models/pcn_cd.py
@@ -1,22 +1,45 @@
 import torch
 import torch.nn as nn
 
-class PCN(nn.Module):
+import json
+from easydict import EasyDict as edict
+
+class PCNEncoder(nn.Module):
     def __init__(self, config):
-        super(PCN, self).__init__()
+        super(PCNEncoder, self).__init__()
         self.config = config
         
+        self.layer1 = nn.Sequential(
+                nn.Conv1d(3, 128, kernel_size=1),
+                nn.ReLU(),
+                nn.Conv1d(128, 256, kernel_size=1),
+                nn.ReLU(),
+            )
 
-    def encode(self, x):
-        pass
-
-    def decode(self, x):
-        pass
+        self.layer2 = nn.Sequential(
+                nn.Conv1d(512, 1024, kernel_size=1),
+                nn.ReLU(),
+            )
 
     def forward(self, x):
-        pass
-
-
+        x = self.layer1(x)
+        print("layer1_out:", x.shape) # TODO: Remove
+        
+        x_global = torch.max(x, 2, keepdim=True)[0]
+        print("x_global:", x_global.shape) # TODO: Remove
+        
+        x_global_repeated = x_global.repeat(1, 1, x.shape[2])
+        print("x_global_repeated:", x_global_repeated.shape)
+        
+        x = torch.cat((x, x_global_repeated), 1)
+        print("x_concatenated", x.shape) # TODO: Remove
+        
+        x = self.layer2(x)
+        print("layer2_out:", x.shape) # TODO: Remove
+        
+        x = torch.max(x, 2)[0]
+        print("out:", x.shape)
+        return x
 
 
 if __name__ == '__main__':
@@ -24,8 +47,8 @@ if __name__ == '__main__':
     config = json.load(open('../../configs/pcn.json'))
     config = edict(config)
     
-    #inp = torch.autograd.Variable(torch.randn(1, 200, 1, 1, 1))
-    #print("input shape:", inp.shape)
-    #g_net = Generator(config)
-    #out = g_net(inp)
-    #print("output shape:", out.shape)
+    input_points = torch.autograd.Variable(torch.randn(32, 2048, 3))
+    input_points = input_points.transpose(2, 1)
+    print("input shape:", input_points.shape)
+    network = PCNEncoder(config) 
+    out = network(input_points)
